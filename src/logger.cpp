be7663aadc0ed3f990115a03af919ed009c8f1de
HaipoviyPodrostok <alexkurteev228@mail.ru>
2025-07-16 12:52:05 +0300

    simple version maybe works

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdarg.h>
+#include <time.h>
 
 #include "logger.h"
-#include "../utils/error_handling.h"
 
 static FILE* log_file = NULL;
 
-logger_err_t log_open(const char* file_name) {
-    assert(file_name);
+logger_err_t log_open(const char* log_file_name) {
+    assert(log_file_name);
     
-    log_file = fopen(file_name, "a");
+    log_file = fopen(log_file_name, "a");
     if (!log_file) return LOGGER_ERR_FILE_OPEN_FAILED;  
 
     return LOGGER_ERR_SUCCES;
@@ -18,10 +19,29 @@ logger_err_t log_open(const char* file_name) {
 
 logger_err_t log_close(void) {
     fclose(log_file);
+    log_file = NULL;
+
     return LOGGER_ERR_SUCCES;
 }
 
-logger_err_t LOG(const char* message, ) {
-    fprintf(log_file, "%s:%d, %s(): %s\n", __FILE__, __LINE__, __func__, message); 
+logger_err_t log_message(const char* log_type, const char* file_from, int line_from, const char* func_from, const char* fmt, ...) {
+    if (!log_file) return LOGGER_ERR_FILE_PRINT_FAILED;
 
+    const char* cur_time = current_time();
+
+    va_list args;
+    va_start(args, fmt);
+    fprintf(log_file, "[%s] %s:%d, %s(): ", cur_time, file_from, line_from, func_from);
+
+    vfprintf(log_file, fmt, args);
+    va_end(args);
+
+    return LOGGER_ERR_SUCCES;
+}
+
+const char* current_time(void) {
+    const time_t cur_time = time(NULL);
+    const char* cur_time_str = ctime(&cur_time);
+
+    return cur_time_str;
 }
--- src/logger.h
+++ src/logger.h
@@ -1,5 +1,14 @@
 #ifndef SRC_LOGGER_H
 #define SRC_LOGGER_H
 
+#include "../utils/error_handling.h"
+
+#define LOG(...) log_message(__FILE__, __LINE__, __func__, __VA__ARGS__)
+
+logger_err_t log_open(const char* file_name);
+logger_err_t log_close(void);
+logger_err_t log_message(const char* log_type, const char* file_from, int line_from, const char* func_from, const char* fmt, ...);
+const char* current_time(void);
+
 
 #endif //SRC_LOGGER_H
